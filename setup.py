ce14e38a78f89c846f117c4ba873572e4bb0b3b4
UnravelSports [JB] <jors@unravelsports.com>
2025-01-26 13:50:51 +0100

    kloppy 3.16

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     python_requires="~=3.11",
     install_requires=[
         "spektral==1.2.0",
-        "kloppy==3.15.0",
+        "kloppy==3.16.0",
         "tensorflow>=2.14.0;platform_machine != 'arm64' or platform_system != 'Darwin'",
         "tensorflow-macos>=2.14.0;platform_machine == 'arm64' and platform_system == 'Darwin'",
         "keras==2.14.0",
