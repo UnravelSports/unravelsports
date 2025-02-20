838687b8729911f6e7003f74367c564d68a17f42
Mihir Thalanki <mihir.thalanki@gmail.com>
2025-02-20 00:17:54 -0500

    Complete node features implementation

--- unravel/soccer/graphs/features/node_features_pl.py
+++ unravel/soccer/graphs/features/node_features_pl.py
@@ -62,85 +62,43 @@ def compute_node_features_pl(
         x=x, y=y, team=team, ball_id=ball_id
     )
 
-    # x_normed = normalize_between(
-    #     value=x,
-    #     max_value=settings.pitch_dimensions.x_dim.max,
-    #     min_value=settings.pitch_dimensions.x_dim.min,
-    # )
-    # y_normed = normalize_between(
-    #     value=y,
-    #     max_value=settings.pitch_dimensions.y_dim.max,
-    #     min_value=settings.pitch_dimensions.y_dim.min,
-    # )
-    # s_normed = normalize_speeds_nfl(s, team, ball_id=ball_id, settings=settings)
-    # uv_velocity = unit_vectors(velocity)
-
-    # angles = normalize_angles(np.arctan2(uv_velocity[:, 1], uv_velocity[:, 0]))
-    # v_sin_normed = normalize_sincos(np.sin(angles))
-    # v_cos_normed = normalize_sincos(np.cos(angles))
-
-    # dist_to_goal = np.linalg.norm(position - goal_mouth_position, axis=1)
-    # normed_dist_to_goal = normalize_distance(
-    #     value=dist_to_goal, max_distance=max_dist_to_goal
-    # )
-
-    # normed_dist_to_ball = normalize_distance(
-    #     value=dist_to_ball, max_distance=max_dist_to_player
-    # )
-
-    # vec_to_goal = goal_mouth_position - position
-    # angle_to_goal = np.arctan2(vec_to_goal[:, 1], vec_to_goal[:, 0])
-    # goal_sin_normed = normalize_sincos(np.sin(angle_to_goal))
-    # goal_cos_normed = normalize_sincos(np.cos(angle_to_goal))
-
-    # vec_to_ball = ball_position - position
-    # angle_to_ball = np.arctan2(vec_to_ball[:, 1], vec_to_ball[:, 0])
-    # ball_sin_normed = normalize_sincos(np.sin(angle_to_ball))
-    # ball_cos_normed = normalize_sincos(np.cos(angle_to_ball))
-
-    # is_possession_team = np.where(
-    #     team == possession_team, 1, settings.defending_team_node_value
-    # )
-
-    # is_ball = np.where(team == ball_id, 1, 0)
     feature_func_map = {
         'x': {'func': lambda value: value, 'defaults': {'value': x}},
         'x_normed': {'func': normalize_between, 'defaults': {'value': x, 'max_value': settings.pitch_dimensions.x_dim.max, 'min_value': settings.pitch_dimensions.x_dim.min}},
         'y': {'func': lambda value: value, 'defaults': {'value': y}},
         'y_normed': {'func': normalize_between, 'defaults': {'value': y, 'max_value': settings.pitch_dimensions.y_dim.max, 'min_value': settings.pitch_dimensions.y_dim.min}},
         's': {'func': lambda value: value, 'defaults': {'value': s}},
-        's_normed': {'func': normalize_speeds_nfl, 'defaults': {'s': s, 'team': team, 'ball_id': ball_id, 'settings': settings}} 
+        's_normed': {'func': normalize_speeds_nfl, 'defaults': {'s': s, 'team': team, 'ball_id': ball_id, 'settings': settings}},
+        'velocity': {'func': lambda value: value, 'defaults': {'value': velocity}},
+        'v_sin_normed': {'func': normalize_sincos, 'defaults': {'value': np.sin(normalize_angles(np.arctan2(unit_vectors(velocity)[:, 1],  unit_vectors(velocity)[:, 0])))}},
+        'v_cos_normed': {'func': normalize_sincos, 'defaults': {'value': np.cos(normalize_angles(np.arctan2(unit_vectors(velocity)[:, 1],  unit_vectors(velocity)[:, 0])))}},
+        'dist_to_goal': {'func': lambda value: value, 'defaults': {'value': np.linalg.norm(position - goal_mouth_position, axis=1)}},
+        'normed_dist_to_goal': {'func': normalize_distance, 'defaults': {'value': np.linalg.norm(position - goal_mouth_position, axis=1), 'max_distance': max_dist_to_goal}},
+        'normed_dist_to_ball': {'func': normalize_distance, 'defaults': {'value': dist_to_ball, 'max_distance': max_dist_to_player}},
+        'vec_to_goal': {'func': lambda value: value, 'defaults': {'value': goal_mouth_position - position}},
+        'angle_to_goal': {'func': lambda value: value, 'defaults': {'value': np.arctan2((goal_mouth_position - position)[:, 1], (goal_mouth_position - position)[:, 0])}},
+        'goal_sin_normed': {'func': normalize_sincos, 'defaults': {'value': np.sin(np.arctan2((goal_mouth_position - position)[:, 1], (goal_mouth_position - position)[:, 0]))}},
+        'goal_cos_normed': {'func': normalize_sincos, 'defaults': {'value': np.cos(np.arctan2((goal_mouth_position - position)[:, 1], (goal_mouth_position - position)[:, 0]))}},
+        'vec_to_ball': {'func': lambda value: value, 'defaults': {'value': ball_position - position}},
+        'angle_to_ball': {'func': lambda value: value, 'defaults': {'value': np.arctan2((ball_position - position)[:, 1], (ball_position - position)[:, 0])}},
+        'ball_sin_normed': {'func': normalize_sincos, 'defaults': {'value': np.sin(np.arctan2((ball_position - position)[:, 1], (ball_position - position)[:, 0]))}},
+        'ball_cos_normed': {'func': normalize_sincos, 'defaults': {'value': np.cos(np.arctan2((ball_position - position)[:, 1], (ball_position - position)[:, 0]))}},
+        'ball_carrier': {'func': lambda value: value, 'defaults': {'value': ball_carrier}},
+        'is_possession_team': {'func': lambda value: value, 'defaults': {'value': np.where(team == possession_team, 1, settings.defending_team_node_value)}},
+        'is_ball': {'func': lambda value: value, 'defaults': {'value': np.where(team == ball_id, 1, 0)}},
+        'is_gk': {'func': lambda value: value, 'defaults': {'value': is_gk}},
     }
+    
     computed_features = []
+    
     for feature, custom_params in feature_dict.items():
         if feature in feature_func_map:
             params = feature_func_map[feature]['defaults'].copy()
             params.update(custom_params)
             computed_features.append(feature_func_map[feature]['func'](**params))
+    
     X = np.nan_to_num(np.stack(computed_features, axis=-1))
-    # X = np.nan_to_num(
-    #     np.stack(
-    #         (
-    #             x_normed,
-    #             y_normed,
-    #             s_normed,
-    #             v_sin_normed,
-    #             v_cos_normed,
-    #             normed_dist_to_goal,
-    #             normed_dist_to_ball,
-    #             is_possession_team,
-    #             is_gk,
-    #             is_ball,
-    #             goal_sin_normed,
-    #             goal_cos_normed,
-    #             ball_sin_normed,
-    #             ball_cos_normed,
-    #             ball_carrier,
-    #         ),
-    #         axis=-1,
-    #     )
-    # )
-
+    
     if graph_features is not None:
         eg = np.ones((X.shape[0], graph_features.shape[0])) * 0.0
         eg[ball_index] = graph_features
